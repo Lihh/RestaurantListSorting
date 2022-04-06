2330173c98d059475a192a109413f38100db7e6f
Lidia Chou <lidia.chou94@gmail.com>
2022-04-06 00:19:44 -0300

    Initial Commit

---
+++ RestaurantListSorting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantListSorting
+//
+//  Created by Lidia Chou on 06/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
