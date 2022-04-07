f9d3b5463826d3b3828e00fc1f997dc2498f94e4
Lidia Chou <lidia.chou94@gmail.com>
2022-04-07 01:23:37 -0300

    Add missing tests and improve README

--- RestaurantListSortingTests/RestaurantListViewModelTests.swift
+++ RestaurantListSortingTests/RestaurantListViewModelTests.swift
@@ -11,7 +11,10 @@ import Quick
 
 class RestaurantListViewModelTests: QuickSpec {
     
+    // MARK: - Properties
     var subject: RestaurantListViewModel = RestaurantListViewModel()
+    
+    // MARK: - Mocked Properties
     let mockedRestaurantListOpen = [Restaurant(name: "Restaurant1", status: "open", sortingValues: SortingValues(bestMatch: 1.0, newest: 1.0, ratingAverage: 1.0, popularity: 1.0, distance: 1, averageProductPrice: 1, deliveryCosts: 1, minCost: 1))]
     let mockedRestaurantListOrderAhead = [Restaurant(name: "Restaurant2", status: "order ahead", sortingValues: SortingValues(bestMatch: 2.0, newest: 2.0, ratingAverage: 2.0, popularity: 2.0, distance: 2, averageProductPrice: 2, deliveryCosts: 2, minCost: 2))]
     let mockedRestaurantListClosed = [Restaurant(name: "Restaurant3", status: "closed", sortingValues: SortingValues(bestMatch: 3.0, newest: 3.0, ratingAverage: 3.0, popularity: 3.0, distance: 3, averageProductPrice: 3, deliveryCosts: 3, minCost: 3))]
@@ -38,9 +41,34 @@ class RestaurantListViewModelTests: QuickSpec {
                 expect(self.subject.getTitleForHeaderInSection(section: 3)).to(equal(""))
             }
             
-            it("Should validate number of rows in sections") {
+            it("Should validate number of rows in sections for openingStateSelected = All") {
+                self.subject.openingStateSelected = .All
                 expect(self.subject.getNumberOfRowsInSection(0)).to(equal(1))
                 expect(self.subject.getNumberOfRowsInSection(1)).to(equal(1))
+                expect(self.subject.getNumberOfRowsInSection(2)).to(equal(1))
+                expect(self.subject.getNumberOfRowsInSection(3)).to(equal(0))
+            }
+            
+            it("Should validate number of rows in sections for openingStateSelected = Open") {
+                self.subject.openingStateSelected = .Open
+                expect(self.subject.getNumberOfRowsInSection(0)).to(equal(1))
+                expect(self.subject.getNumberOfRowsInSection(1)).to(equal(0))
+                expect(self.subject.getNumberOfRowsInSection(2)).to(equal(0))
+                expect(self.subject.getNumberOfRowsInSection(3)).to(equal(0))
+            }
+            
+            it("Should validate number of rows in sections for openingStateSelected = OrderAhead") {
+                self.subject.openingStateSelected = .OrderAhead
+                expect(self.subject.getNumberOfRowsInSection(0)).to(equal(0))
+                expect(self.subject.getNumberOfRowsInSection(1)).to(equal(1))
+                expect(self.subject.getNumberOfRowsInSection(2)).to(equal(0))
+                expect(self.subject.getNumberOfRowsInSection(3)).to(equal(0))
+            }
+            
+            it("Should validate number of rows in sections for openingStateSelected = Closed") {
+                self.subject.openingStateSelected = .Closed
+                expect(self.subject.getNumberOfRowsInSection(0)).to(equal(0))
+                expect(self.subject.getNumberOfRowsInSection(1)).to(equal(0))
                 expect(self.subject.getNumberOfRowsInSection(2)).to(equal(1))
                 expect(self.subject.getNumberOfRowsInSection(3)).to(equal(0))
             }
